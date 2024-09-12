6fb6dfad2e13cf8a26674699f969fa905e0fa8cc
Danilo lenardi de Almeida <danlenardial@gmail.com>
2024-09-11 23:03:26 -0300

    aula_1.4

--- index_2.js
+++ index_2.js
@@ -2,15 +2,25 @@
 let meta = {
     value: "ler um livro todo mês",
     checked: false,
-    log: (info) => {
+   /* log: (info) => {
         console.log(info)
-    } 
+    }*/ 
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos por dia",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
+
+//meta.value = "não é mais ler um livro"
+//meta.log(meta.value)
 
 //function // arrow function
 
-const CriarMeta = () => {}
+//const CriarMeta = () => {}
 // function CriarMeta () {}
